1d1247f29b726369e92bdccbf523e58eded16a0f
affishaikh <sk.aftab1999@gmail.com>
2020-03-18 23:54:19 +0530

    Start and End Race

--- src/typing_racer_client/core.cljs
+++ src/typing_racer_client/core.cljs
@@ -11,11 +11,19 @@
 (defn mount-element [id body]
   (reagent/render-component [(fn [] body)] (gdom/getElement id)))
 
+(defn start-race []
+  (http/post "http://localhost:9002/start-race" {:with-credentials? false}))
+
+(defn end-race []
+  (http/post "http://localhost:9002/end-race" {:with-credentials? false}))
+
 (go (let [response (<! (http/get "http://localhost:9002/paragraph" {:with-credentials? false}))]
       (mount-element "main"
                      [:div
-                    [:div {:class "paragraph"} [:p (:body response)]]
-                    [:div [:input]]])))
+                      [:div {:class "paragraph"} [:p (:body response)]]
+                      [:div [:input]]
+                      [:button {:onClick start-race} "start"]
+                      [:button {:onClick end-race} "end"]])))
 
 (mount-element "app" title)
 

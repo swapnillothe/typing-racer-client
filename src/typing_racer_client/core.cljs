904c77e93c47744b16d92247e427116d45156dce
affishaikh <sk.aftab1999@gmail.com>
2020-03-20 11:35:24 +0530

    Calculate the words typed

--- src/typing_racer_client/core.cljs
+++ src/typing_racer_client/core.cljs
@@ -3,6 +3,7 @@
   (:require
     [goog.dom :as gdom]
     [reagent.core :as reagent :refer [atom]]
+    [clojure.string :as str]
     [cljs-http.client :as http]
     [cljs.core.async :refer [<!]]))
 
@@ -14,16 +15,27 @@
 (defn start-race []
   (http/post "http://localhost:9002/start-race" {:with-credentials? false}))
 
-(defn end-race []
-  (http/post "http://localhost:9002/end-race" {:with-credentials? false}))
+(defn end-race [typed-text]
+  (http/post "http://localhost:9002/end-race"
+             {:with-credentials? false
+              :form-params       {:words-count (if
+                                                 (= 0 (count typed-text))
+                                                 0
+                                                 (count (str/split typed-text #" ")))}}))
+
+(defn typing-area
+  [typed-text]
+  [:div [:input {:type     "text" :value @typed-text
+                 :onChange #(reset! typed-text (.-value (.-target %)))}]])
 
 (go (let [response (<! (http/get "http://localhost:9002/paragraph" {:with-credentials? false}))]
       (mount-element "main"
-                     [:div
-                      [:div {:class "paragraph"} [:p (:body response)]]
-                      [:div [:input]]
-                      [:button {:onClick start-race} "start"]
-                      [:button {:onClick end-race} "end"]])))
+                     (let [typed-text (atom "")]
+                       [:div
+                        [:div {:class "paragraph"} [:p (:body response)]]
+                        [typing-area typed-text]
+                        [:button {:onClick start-race} "start"]
+                        [:button {:onClick #(end-race @typed-text)} "end"]]))))
 
 (mount-element "app" title)
 
